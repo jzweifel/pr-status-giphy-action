4a867c2abc69c29cdf1ab4bb61d21e4c968f51e8
Jacob Zweifel <jzweifel@tribalscale.com>
2019-01-27 18:01:25 -0500

    write out the PR number if the PR is open or updated

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const NEUTRAL_ERROR_CODE = process.env.GITHUB_WORKFLOW ? 78 : 0;
+
 /* eslint no-console: 0 */
 console.log("Hello World!");
+
+const githubEventPath = process.env.GITHUB_EVENT_PATH || "";
+
+const githubEvent = githubEventPath ? require(githubEventPath) : "";
+
+if (
+  !githubEvent &&
+  githubEvent.action != "synchronize" &&
+  githubEvent.action != "opened"
+)
+  process.exit(NEUTRAL_ERROR_CODE);
+
+console.log(`Pull Request #${githubEvent.number}`);
