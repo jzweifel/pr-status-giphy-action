d56c069067688ab3d363fd8584e0eed87cd50652
Jacob Zweifel <jzweifel@tribalscale.com>
2019-01-28 09:53:28 -0500

    Run when the PR is Open or Updated (#3)
    * write out the PR number if the PR is open or updated
    
    * self reference the action in the PR workflow
    
    * set additional flags in entrypoint
    
    * add additional logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Exit if any subcommand fails
-set -e
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -eu
 
 sh -c "node index.js $*"
--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 /* eslint no-console: 0 */
-console.log("Hello World!");
+const NEUTRAL_ERROR_CODE = process.env.GITHUB_WORKFLOW ? 78 : 0;
+
+const githubEventPath = process.env.GITHUB_EVENT_PATH || "";
+const githubAction = process.env.GITHUB_ACTION || "";
+
+const githubEvent = githubEventPath ? require(githubEventPath) : "";
+
+if (
+  !githubEvent ||
+  (githubEvent.action !== "synchronize" && githubEvent.action !== "opened")
+) {
+  console.log(
+    `GitHub event payload not found or Pull Request event does not have desired action. Action was ${
+      githubEvent.action
+    }.`
+  );
+  process.exit(NEUTRAL_ERROR_CODE);
+}
+
+console.log(
+  `Running ${githubAction} for Pull Request #${
+    githubEvent.number
+  } triggered by action ${githubEvent.action}.`
+);
